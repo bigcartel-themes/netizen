da486e0d95bc1a900f114f1ac3bcf3f4267c90e5
Trevin Chow <trevin@bigcartel.com>
2025-10-31 16:35:11 -0700

    chore: improve BNPL messaging performance
    - reduce layout shift
    - improve performance
    - add debug logging capabillity

--- source/javascripts/store.js
+++ source/javascripts/store.js
@@ -528,7 +528,7 @@ function disableSelectOption(select_option, type) {
   if (type === "sold-out") {
     disabled_text = select_option.parent().attr("data-sold-text");
     disabled_type = "sold-out";
-    if (show_sold_out_product_options === 'false') {
+    if (themeOptions.showSoldOutOptions === false) {
       hide_option = true;
     }
     else {
